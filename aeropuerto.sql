9dd710a1ba8d9b7ffc4e1e8e1071017dff02517f
juans <juansebastianchicagiraldo@gmail.com>
2021-11-08 17:06:42 -0500

    CRUD Aeropuerto

--- aeropuerto.sql
+++ aeropuerto.sql
@@ -1,231 +1,273 @@
-/*Comando que se utiliza para crea una nueva base de datos*/
-CREATE DATABASE aeropuerto
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-11-2021 a las 22:48:34
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-/*Comando que se utiliza para seleccionar la base de datos*/
-USE aeropuerto
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `aeropuerto`
+--
 
-/*Estructura para crear una tabla con el comando create table*/
-CREATE TABLE persona(
-		cod_persona int(11) AUTO_INCREMENT NOT NULL,/*Campo autoincrementable*/
-		doc_persona VARCHAR(50) NOT NULL,
-		tipo_documento VARCHAR(50) NOT NULL,
-		nombre_persona varchar(50) NOT NULL,
-		apellido_persona varchar(50) NOT NULL,
-		fecha_nac date NOT NULL,
-		sexo VARCHAR(50) NOT NULL,
-		celular varchar(10) NOT NULL,
-		correo varchar(50) NOT NULL,
-		PRIMARY KEY(cod_persona))/*Primary key se utiliza para asignar llave la llave primaria como campo unico*/
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `avion`
+--
 
-/*Sentencia sql que se utiliza para registrar informacióon en una tabla con el comando insert into*/
-	INSERT INTO persona (cod_persona, doc_persona, tipo_documento, nombre_persona, apellido_persona, fecha_nac, sexo, celular, correo,fecha_reg) VALUES
-	(1, '1094955221', 'Cedula', 'Juan Sebastian', 'Chica Giraldo', '1996-02-13', 'H', '3235807900', 'juansebastianchicagiraldo@gmail.com',NOW()),
-	(2, '1093945332', 'Cedula', 'Juan Jose', 'Ospina Zapata', '1994-12-20', 'H', '3245678909', 'jjose20@gmail.com',NOW()),
-	(3, '9602132010', 'Ti', 'Manuel Jose', 'Garcia Rivera', '2004-08-18', 'H', '3234567890', 'juansebastianchicagiraldo@gmail.com',NOW()),
-	(4, '1092934599', 'Cedula', 'Maria Cristina', 'Rivera Giraldo', '1995-01-19', 'M', '3234568879', 'mariac@gmail.com',NOW()),
-	(5, '1093456485', 'Cedula', 'Maria Fernanda', 'Ospina Garcia', '1994-11-12', 'M', '3234567890', 'marifer@gmail.com',NOW())
+CREATE TABLE `avion` (
+  `cod_avion` int(11) NOT NULL,
+  `capacidad` varchar(255) DEFAULT NULL,
+  `descipcion` varchar(255) DEFAULT NULL,
+  `marca` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
-/*Sentencia para hacer un cambio a una tabla en este caso se añade una columna nueva fecha_reg para registar la 
-fecha actual con datetime*/
-	 	ALTER TABLE persona ADD fecha_reg DATETIME
-	 	
-/*El comando select se utiliza para recuperar datos de una o más tablas*/
-		SELECT * FROM persona
-	
-/*La sentencia update se utiliza para modificar valores en una tabla.*/
-		UPDATE persona SET tipo_documento = 'Ti' WHERE cod_persona = 5
-	
-/*Sentencia que me permite contar  todos los registros, se guarda con un alis utilizando as por ultimo 
-se agrupan por sexo*/
-		SELECT sexo , count(*) as cantidad FROM persona GROUP BY sexo
-	
-/*Sentencia que se usa para devolver valores con un campo en especifico utilizando condicion where y la funcion
-lower hace minuscula un campo */
-		SELECT * FROM persona WHERE LOWER(tipo_documento)= 'Cedula'
-	
-/*La palabra clave ASC se usa para ordenar el conjunto de resultados de la consulta en orden ascendente*/
-		SELECT * FROM persona ORDER BY nombre_persona ASC
-		
+--
+-- Estructura de tabla para la tabla `estado`
+--
 
-/*Estructura para crear una tabla con el comando create table*/
-CREATE TABLE marca(
-		cod_marca INT(11) AUTO_INCREMENT NOT NULL,
-		nombre VARCHAR(50) NOT NULL,
-		PRIMARY KEY(cod_marca))/*Primary key se utiliza para asignar llave la llave primaria como campo unico*/
-		 
-		
-/*Sentencia sql que se utiliza para registrar informacióon en una tabla con el comando insert into*/
-	INSERT INTO marca (cod_marca,nombre) VALUES
-	(1, 'Boeing 747'),
-	(2, 'Boeing 777'),
-	(3, 'Airbus A340'),
-	(4,  'Airbus A330'),
-	(5, 'Boeing 757')
-	
+CREATE TABLE `estado` (
+  `cod_estado` int(11) NOT NULL,
+  `estado` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-/*El comando select se utiliza para recuperar datos de una o más tablas*/
-		SELECT * FROM marca
-	
-/*La sentencia update se utiliza para modificar valores en una tabla.*/
-		UPDATE marca SET nombre = 'Boeing 737' WHERE cod_marca = 5
-	
-/*Sentencia que busca todos los registros con la palabra Boeing y cuenta cuantos registros existen*/
-		SELECT nombre AS marca,count(*) as cantidad FROM marca WHERE nombre LIKE '%Boeing%'
-		
+--
+-- Volcado de datos para la tabla `estado`
+--
 
-/*Sentencia que selecciona todos los registros que tengan la palabra Boeing*/		
-		SELECT * FROM marca WHERE nombre LIKE '%Boeing%'
+INSERT INTO `estado` (`cod_estado`, `estado`) VALUES
+(1, 'Despegue'),
+(2, 'Aterrizaje');
 
-/*Sentencia que se usa para contar los registros de una tabla*/		
-SELECT COUNT(*) as cantidad FROM marca
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `factura`
+--
 
-/*Estructura para crear una tabla con el comando create table*/
-CREATE TABLE avion(
-		cod_avion INT(11) AUTO_INCREMENT NOT NULL,
-		descripcion TEXT NOT NULL,
-		capacidad VARCHAR (50) NOT NULL,
-		marca INT(11) NOT NULL,
-		CONSTRAINT fk_Marca FOREIGN KEY (marca) REFERENCES marca (cod_marca),/*Estructura que relaciona 
-		las tablas avion y marca con la llave foranea marca que pertenece a la tabla avion 
-		y cod_marca como llave primaria que perteneciente a la tabla marca*/	
-		
-/*Sentencia sql que se utiliza para registrar informacióon en una tabla con el comando insert into*/	
-	INSERT INTO avion (cod_avion,descripcion,capacidad,marca) VALUES
-	(1, 'es de los aviones más reconocibles del mundo','467 pasajeros',1),
-	(2, 'El Boeing 777 es un avión jet bimotor, de fuselaje ancho y de largo alcance manufacturado por la Boeing','368 pasajeros',2),
-	(3, 'El avión Airbus A340 es un avión jet comercial de pasajeros de largo alcance de fuselaje ancho y cuatro motores','370 pasajeros',3),
-	(4,  'El Airbus A330 es un avión jet de fuselaje ancho bimotor construido por Airbus','288 pasajeros',4),
-	(5, 'El Boeing 757 es un avión jet de fuselaje estrecho de dos motores contruido por la compañía Boeing ','200 pasajeros',5)	
+CREATE TABLE `factura` (
+  `cod_factura` int(11) NOT NULL,
+  `asiento` varchar(255) DEFAULT NULL,
+  `clase` varchar(255) DEFAULT NULL,
+  `cod_vuelo` int(11) NOT NULL,
+  `fecha_reg` date DEFAULT NULL,
+  `total` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-/*Los join sirven para combinar filas de dos o más tablas basándose en un campo común entre ellas*/
-		SELECT nombre AS marca ,descripcion FROM avion INNER JOIN marca ON marca.cod_marca = avion.marca
-		
-/*Estructura para crear una tabla con el comando create table*/	
-CREATE TABLE origen(
-		cod_origen INT(11) AUTO_INCREMENT NOT NULL,
-		origen VARCHAR	(50),
-		PRIMARY KEY(cod_origen))/*Primary key se utiliza para asignar llave la llave primaria como campo unico*/	
+--
+-- Volcado de datos para la tabla `factura`
+--
 
-/*Sentencia sql que se utiliza para registrar informacióon en una tabla con el comando insert into*/	
-	INSERT INTO origen (cod_origen ,origen) VALUES
-	(1, 'Armenia'),
-	(2, 'Bogota'),
-	(3, 'Cali'),
-	(4, 'Pereira'),
-	(5, 'Bucaramanga')   
-		
-/*Estructura para crear una tabla con el comando create table*/
-CREATE TABLE destino(
-		cod_destino int(11) AUTO_INCREMENT NOT NULL,
-		destino VARCHAR	(50),
-		PRIMARY KEY(cod_destino))/*Primary key se utiliza para asignar llave la llave primaria como campo unico*/
-		
-/*Sentencia sql que se utiliza para registrar informacióon en una tabla con el comando insert into*/   
-	INSERT INTO destino (cod_destino ,destino) VALUES
-	(1, 'Ibague'),
-	(2, 'Barranquilla'),
-	(3, 'Cartagena'),
-	(4, 'Manizales'),
-	(5, 'Pasto')  
-		
-/*Estructura para crear una tabla con el comando create table*/
-CREATE TABLE estado(
-		cod_estado INT(11) AUTO_INCREMENT NOT NULL,
-		estado VARCHAR(50),
-		PRIMARY KEY(cod_estado))/*Primary key se utiliza para asignar llave la llave primaria como campo unico*/
-		
-/*Sentencia sql que se utiliza para registrar informacióon en una tabla con el comando insert into*/ 
-	INSERT INTO estado (cod_estado ,estado) VALUES
-	(1, 'Despegue'),
-	(2, 'Aterrizaje')
-		
+INSERT INTO `factura` (`cod_factura`, `asiento`, `clase`, `cod_vuelo`, `fecha_reg`, `total`) VALUES
+(1, '405B', 'VIP', 2, '2021-11-08', '350.150');
 
-/*Estructura para crear una tabla con el comando create table*/
-CREATE TABLE vuelo(
-		cod_vuelo INT(11) AUTO_INCREMENT NOT NULL,
-		cod_origen INT(11) NOT NULL,
-		cod_destino INT(11) NOT NULL,
-		cod_estado INT(11) NOT NULL,
-		cod_marca INT(11) NOT NULL,
-		cod_persona INT(11) NOT NULL,
-		CONSTRAINT fk_Origen FOREIGN KEY (cod_origen) REFERENCES origen (cod_origen),/*Estructura que relaciona las 
-		tablas origen y vuelo con la llave foranea cod_origen que pertenece a la tabla vuelo 
-		y cod_origen como llave primaria que perteneciente a la tabla origen*/
-		CONSTRAINT fk_Destino FOREIGN KEY (cod_destino) REFERENCES destino (cod_destino),/*Estructura que relaciona 
-		las tablas destino y vuelo con la llave foranea cod_destino que pertenece a la tabla vuelo 
-		y cod_destino  como llave primaria que perteneciente a la tabla destino*/
-		CONSTRAINT fk_Estado FOREIGN KEY (cod_estado) REFERENCES estado (cod_estado),/*Estructura que relaciona las 
-		tablas estado y vuelo con la llave foranea cod_estado que pertenece a la tabla vuelo 
-		y cod_estado como llave primaria que perteneciente a la tabla estado*/
-		CONSTRAINT fk_Marcas FOREIGN KEY (cod_marca) REFERENCES marca (cod_marca),/*Estructura que relaciona las 
-		tablas marca y vuelo con la llave foranea cod_marca que pertenece a la tabla vuelo 
-		y cod_marca como llave primaria que perteneciente a la tabla marca*/
-		CONSTRAINT fk_Persona FOREIGN KEY (cod_persona) REFERENCES persona (cod_persona),/*Estructura que relaciona 
-		las tablas persona y vuelo con la llave foranea cod_persona que pertenece a la tabla vuelo 
-		y cod_persona como llave primaria que perteneciente a la tabla persona*/
-		PRIMARY KEY(cod_vuelo))/*Primary key se utiliza para asignar llave la llave primaria como campo unico*/
+-- --------------------------------------------------------
 
-/*Sentencia sql que se utiliza para registrar informacióon en una tabla con el comando insert into*/ 
-	INSERT INTO vuelo (cod_vuelo, cod_origen,cod_destino,cod_estado,cod_marca,cod_persona) VALUES 
-	(1, 5, 4, 2, 3, 1),
-	(2, 3, 3, 1, 5, 3),
-	(3, 4, 2, 1, 2, 4),
-	(4, 2, 1, 2, 2, 2),
-	(5, 1, 5, 2, 1, 5)
-		
-/*Estructura para crear una tabla con el comando create table*/
-CREATE TABLE factura(
-		cod_factura INT(11) AUTO_INCREMENT NOT NULL,
-		total DECIMAL (20,3),
-		asiento VARCHAR (50),
-		clase VARCHAR (50),
-		fecha_reg DATETIME,
-		cod_vuelo INT(11),
-	   CONSTRAINT fk_vuelo FOREIGN KEY (cod_vuelo) REFERENCES vuelo (cod_vuelo),/*Estructura que relaciona 
-		las tablas factura y vuelo con la llave foranea cod_vuelo que pertenece a la tabla vuelo 
-		y cod_factura como llave primaria que perteneciente a la tabla factura*/
-		PRIMARY KEY(cod_factura))/*Primary key se utiliza para asignar llave la llave primaria como campo unico*/	
-		
-		
-/*Sentencia sql que se utiliza para registrar informacióon en una tabla con el comando insert into*/ 
+--
+-- Estructura de tabla para la tabla `marca`
+--
 
-	INSERT INTO factura (cod_factura, total,asiento,clase,fecha_reg,cod_vuelo,descripcion) VALUES 
-	(1, 550.000, '23A', 'Clase Economica', NOW(), 1,'Vuelo con acompañante'),
-	(2, 760.250, '15K', 'Clase Ejecutiva', NOW(), 2,'Vuelo con escala'),
-	(3, 450.000, '18E', 'Primera Clase', now(), 4,'Vuelo sin escala'),
-	(4, 320.000, '10A', 'Clase Economica', NOW(), 2,'Vuelo directo'),
-	(5, 980.350, '30D', 'Clase Ejecutiva', NOW(), 5,'Vuelo con fuertes lluvias')	
+CREATE TABLE `marca` (
+  `cod_marca` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-/*El comando select se utiliza para recuperar datos de una o más tablas*/
-		SELECT * FROM factura
-	
-/*Sentencia que se usa para devolver valores con un campo en especifico utilizando condicion where y la funcion
-lower hace minuscula un campo*/
-		SELECT nombre_persona,total,asiento,clase,origen,destino ,estado,fecha_reg FROM factura INNER JOIN vuelo ON 
-		vuelo.cod_vuelo = factura.cod_vuelo INNER JOIN persona ON persona.cod_persona = vuelo.cod_vuelo INNER JOIN estado ON 
-		estado.cod_estado = vuelo.cod_estado INNER JOIN origen ON origen.cod_origen = vuelo.cod_origen  
-		INNER JOIN destino ON destino.cod_destino = vuelo.cod_destino WHERE LOWER(estado)= 'aterrizaje' ORDER BY nombre_persona
-		
-/*Estructura que devuelve registros existentes con tablas relacionadas y se utiliza inner join*/
-		SELECT nombre_persona,total,asiento,clase,origen,destino ,estado,fecha_reg FROM factura INNER JOIN vuelo ON 
-		vuelo.cod_vuelo = factura.cod_vuelo INNER JOIN persona ON persona.cod_persona = vuelo.cod_vuelo INNER JOIN estado ON 
-		estado.cod_estado = vuelo.cod_estado INNER JOIN origen ON origen.cod_origen = vuelo.cod_origen  
-		INNER JOIN destino ON destino.cod_destino = vuelo.cod_destino ORDER BY nombre_persona
-		
-/*Consulta que se utiliza para obtener el valor a pagar mas alto para ello se utiliza la clausula max*/
-		SELECT nombre_persona,MAX(total) AS total,asiento,clase,origen,destino ,estado,fecha_reg FROM factura INNER JOIN vuelo ON 
-		vuelo.cod_vuelo = factura.cod_vuelo INNER JOIN persona ON persona.cod_persona = vuelo.cod_vuelo INNER JOIN estado ON 
-		estado.cod_estado = vuelo.cod_estado INNER JOIN origen ON origen.cod_origen = vuelo.cod_origen  
-		INNER JOIN destino ON destino.cod_destino = vuelo.cod_destino ORDER BY nombre_persona
-		
-/*Consulta que se utiliza para obtener el valor a pagar mas bajo para ello se utiliza la clausula max*/
-		SELECT nombre_persona,MIN(total) AS total,asiento,clase,origen,destino ,estado,fecha_reg FROM factura INNER JOIN vuelo ON 
-		vuelo.cod_vuelo = factura.cod_vuelo INNER JOIN persona ON persona.cod_persona = vuelo.cod_vuelo INNER JOIN estado ON 
-		estado.cod_estado = vuelo.cod_estado INNER JOIN origen ON origen.cod_origen = vuelo.cod_origen  
-		INNER JOIN destino ON destino.cod_destino = vuelo.cod_destino ORDER BY nombre_persona	
+--
+-- Volcado de datos para la tabla `marca`
+--
 
+INSERT INTO `marca` (`cod_marca`, `nombre`) VALUES
+(1, 'Prueba');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `cod_persona` int(11) NOT NULL,
+  `apellido_persona` varchar(255) DEFAULT NULL,
+  `celular` varchar(255) DEFAULT NULL,
+  `correo` varchar(255) DEFAULT NULL,
+  `doc_persona` varchar(255) DEFAULT NULL,
+  `fecha_nac` date DEFAULT NULL,
+  `fecha_reg` date DEFAULT NULL,
+  `nombre_persona` varchar(255) DEFAULT NULL,
+  `sexo` varchar(255) DEFAULT NULL,
+  `tipo_documento` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`cod_persona`, `apellido_persona`, `celular`, `correo`, `doc_persona`, `fecha_nac`, `fecha_reg`, `nombre_persona`, `sexo`, `tipo_documento`) VALUES
+(1, 'Chica Giraldo', '3235807900', 'juan@gmail.com', '1094955221', '1996-02-13', '2021-11-04', 'Juan Sebastian', 'Hombre', 'Cedula');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ruta`
+--
+
+CREATE TABLE `ruta` (
+  `cod_ruta` int(11) NOT NULL,
+  `destino` varchar(255) DEFAULT NULL,
+  `origen` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `ruta`
+--
+
+INSERT INTO `ruta` (`cod_ruta`, `destino`, `origen`) VALUES
+(1, 'Armenia', 'Bogota'),
+(2, 'Pereira', 'Medellin');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vuelo`
+--
+
+CREATE TABLE `vuelo` (
+  `cod_vuelo` int(11) NOT NULL,
+  `cod_estado` int(11) NOT NULL,
+  `cod_marca` int(11) NOT NULL,
+  `cod_persona` int(11) NOT NULL,
+  `cod_ruta` int(11) NOT NULL,
+  `fecha_reg` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `vuelo`
+--
+
+INSERT INTO `vuelo` (`cod_vuelo`, `cod_estado`, `cod_marca`, `cod_persona`, `cod_ruta`, `fecha_reg`) VALUES
+(2, 2, 1, 1, 1, '2021-11-03'),
+(3, 1, 1, 1, 1, '2021-11-02'),
+(5, 2, 1, 1, 1, '2021-11-02'),
+(7, 1, 1, 1, 1, '2021-11-01'),
+(8, 1, 1, 1, 1, '2021-11-02'),
+(9, 1, 1, 1, 1, '2021-11-02'),
+(10, 1, 1, 1, 1, '2021-11-02'),
+(11, 1, 1, 1, 1, '2021-11-02'),
+(12, 1, 1, 1, 1, '2021-11-02');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `avion`
+--
+ALTER TABLE `avion`
+  ADD PRIMARY KEY (`cod_avion`),
+  ADD KEY `FK4xrrgrykxn81hk31l0pa5jkft` (`marca`);
+
+--
+-- Indices de la tabla `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`cod_estado`);
+
+--
+-- Indices de la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD PRIMARY KEY (`cod_factura`),
+  ADD KEY `FK1ag9dg8ej4eu7qaovk230fnwx` (`cod_vuelo`);
+
+--
+-- Indices de la tabla `marca`
+--
+ALTER TABLE `marca`
+  ADD PRIMARY KEY (`cod_marca`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`cod_persona`);
+
+--
+-- Indices de la tabla `ruta`
+--
+ALTER TABLE `ruta`
+  ADD PRIMARY KEY (`cod_ruta`);
+
+--
+-- Indices de la tabla `vuelo`
+--
+ALTER TABLE `vuelo`
+  ADD PRIMARY KEY (`cod_vuelo`),
+  ADD KEY `FKmikp9hl8f46695hnp8fx48tyk` (`cod_ruta`),
+  ADD KEY `FKf5xw9ka1vyu3fmu9m8ilgqy81` (`cod_persona`),
+  ADD KEY `FK9rqtocl71j3dmqwtochk129jh` (`cod_marca`),
+  ADD KEY `FK22sles0xswmhkab6gk155hvqr` (`cod_estado`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `avion`
+--
+ALTER TABLE `avion`
+  MODIFY `cod_avion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `cod_estado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `factura`
+--
+ALTER TABLE `factura`
+  MODIFY `cod_factura` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `marca`
+--
+ALTER TABLE `marca`
+  MODIFY `cod_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `cod_persona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `ruta`
+--
+ALTER TABLE `ruta`
+  MODIFY `cod_ruta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `vuelo`
+--
+ALTER TABLE `vuelo`
+  MODIFY `cod_vuelo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
